a52809a82af817013784260d328372988d93a7f9
jripon1991 <jripon1991@gmail.com>
2018-12-29 18:44:00 +0600

    Reply CRUD

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Model\Reply;
+use App\Model\Question;
 use Illuminate\Http\Request;
 
 class ReplyController extends Controller
@@ -12,9 +13,11 @@ class ReplyController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Question $question)
     {
-        //
+        return $question->replies;
+
+        //return Reply::latest()->get();
     }
 
     /**
@@ -33,9 +36,11 @@ class ReplyController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Question $question, Request $request)
     {
-        //
+        $reply = $question->replies()->create($request->all());
+
+        return response([ 'reply' => $reply ], 201);
     }
 
     /**
@@ -44,9 +49,9 @@ class ReplyController extends Controller
      * @param  \App\Model\Reply  $reply
      * @return \Illuminate\Http\Response
      */
-    public function show(Reply $reply)
+    public function show(Question $question, Reply $reply)
     {
-        //
+        return $reply;
     }
 
     /**
@@ -78,8 +83,10 @@ class ReplyController extends Controller
      * @param  \App\Model\Reply  $reply
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Reply $reply)
+    public function destroy(Question $question, Reply $reply)
     {
-        //
+        $reply->delete();
+
+        return response()->json('Deleted', 204);
     }
 }
